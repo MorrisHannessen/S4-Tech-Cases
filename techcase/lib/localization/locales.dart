ad536a76637e053dca70d850c1eff252fc107cff
Morris Hannessen <510709@student.fontys.nl>
2024-02-29 16:00:22 +0100

    finished app

--- techcase/lib/localization/locales.dart
+++ techcase/lib/localization/locales.dart
@@ -12,28 +12,48 @@ mixin LocaleData {
   static const String title = "title";
   static const String body = "body";
   static const String batteryLevel = "batteryLevel";
+  static const String emptyPhone = "emptyPhone";
+  static const String fullPhone = "fullPhone";
+  static const String hours = "hours";
+  static const String minutes = "minutes";
 
   static const Map<String, dynamic> EN = {
     title: "TechCase 9",
     body: "Welcome to TechCase 9",
     batteryLevel: "Battery Level:",
+    emptyPhone: "Time left till phone is empty:",
+    fullPhone: "Time left till phone is full:",
+    hours: "Hours",
+    minutes: "Minutes",
   };
 
   static const Map<String, dynamic> ES = {
     title: "TechCase 9",
     body: "Bienvenido a TechCase 9",
     batteryLevel: "Nivel de batería:",
+    emptyPhone: "Tiempo restante hasta que el teléfono esté vacío:",
+    fullPhone: "Tiempo restante hasta que el teléfono esté lleno:",
+    hours: "Horas",
+    minutes: "Minutos",
   };
 
   static const Map<String, dynamic> ZH = {
     title: "TechCase 9",
     body: "欢迎来到TechCase 9",
     batteryLevel: "电池电量：",
+    emptyPhone: "手机剩余时间：",
+    fullPhone: "手机充满时间：",
+    hours: "小时",
+    minutes: "分钟",
   };
 
   static const Map<String, dynamic> AR = {
     title: "TechCase 9",
     body: "مرحبًا بك في TechCase 9",
     batteryLevel: "مستوى البطارية:",
+    emptyPhone: "الوقت المتبقي حتى ينفد الهاتف:",
+    fullPhone: "الوقت المتبقي حتى يمتلئ الهاتف:",
+    hours: "ساعات",
+    minutes: "دقائق",
   };
 }
--- techcase/lib/page/home_page.dart
+++ techcase/lib/page/home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 import 'package:techcase/localization/locales.dart';
@@ -14,14 +15,24 @@ class _HomePageState extends State<HomePage> {
   late FlutterLocalization _flutterLocalization;
   var battery = Battery();
   late int _batteryLevel;
+  late BatteryState _batteryState;
   late String _currentLocale;
+  late Timer _timer;
+
   @override
   void initState() {
     super.initState();
     _flutterLocalization = FlutterLocalization.instance;
     _currentLocale = _flutterLocalization.currentLocale!.languageCode;
     _getBatteryLevel();
-    print(_currentLocale);
+    _getBatteryState();
+    _startUpdating(); // Start periodic updates
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel(); // Cancel the timer to avoid memory leaks
+    super.dispose();
   }
 
   @override
@@ -72,6 +83,40 @@ class _HomePageState extends State<HomePage> {
                   " $_batteryLevel%",
               style: TextStyle(fontSize: 18),
             ),
+            SizedBox(height: 20),
+            Container(
+              width: 200,
+              child: LinearProgressIndicator(
+                value: _batteryLevel / 100,
+                minHeight: 10,
+                backgroundColor: Colors.grey[300],
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  _getBatteryColor(_batteryLevel),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              context.formatString(LocaleData.emptyPhone, ["Morris"]) +
+                  " ${calculateTimeToEmpty(_batteryLevel)}",
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 10),
+            FutureBuilder<bool>(
+              future: isCharging(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData && snapshot.data == true) {
+                  return Text(
+                    context.formatString(LocaleData.fullPhone, ["Morris"]) +
+                        " ${calculateTimeToFullCharge(_batteryLevel)}",
+                    style: TextStyle(fontSize: 16),
+                  );
+                } else {
+                  return SizedBox
+                      .shrink(); // Returns an empty widget if not charging
+                }
+              },
+            ),
           ],
         ),
       ),
@@ -96,10 +141,94 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Color _getBatteryColor(int batteryLevel) {
+    if (batteryLevel <= 20) {
+      return Colors.red;
+    } else if (batteryLevel <= 50) {
+      return Colors.orange;
+    } else {
+      return Colors.green;
+    }
+  }
+
+  void _startUpdating() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      _getBatteryLevel();
+      _getBatteryState();
+    });
+  }
+
   Future<void> _getBatteryLevel() async {
     final batteryLevel = await battery.batteryLevel;
     setState(() {
       _batteryLevel = batteryLevel;
     });
+  }
+
+  Future<void> _getBatteryState() async {
+    final batteryState = await battery.batteryState;
+    if (batteryState != _batteryState) {
+      setState(() {
+        _batteryState = batteryState;
+      });
+    }
+  }
+
+  Future<void> _refresh() async {
+    await _getBatteryLevel();
+  }
+
+  Future<bool> isCharging() async {
+    final BatteryState batteryState = await battery.batteryState;
+    if (batteryState == BatteryState.charging) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  String calculateTimeToEmpty(int batteryLevel) {
+    int batteryCapacity = 5000; // mAh
+    int averageConsumption = 100; // mA
+    final hoursToEmpty =
+        (batteryLevel / 100) * batteryCapacity / averageConsumption;
+
+    if (hoursToEmpty.isInfinite || hoursToEmpty.isNaN) {
+      return 'N/A';
+    }
+
+    final hours = hoursToEmpty.floor();
+    final minutes = ((hoursToEmpty - hours) * 60).round();
+
+    return '$hours ' +
+        LocaleData.hours.getString(context) +
+        ' $minutes ' +
+        LocaleData.minutes.getString(context);
+  }
+
+  String calculateTimeToFullCharge(int currentBatteryLevel) {
+    double deviceBatteryCapacity = 5; //Ah
+    double maxChargingVolt = 11; //Volt
+    double maxChargingWatts = 33; //Watts
+    double maxChargingRate = maxChargingWatts / maxChargingVolt; //Ampere
+    double chargingEfficiency = 0.8; //80%
+    if (currentBatteryLevel == 100) {
+      return 'N/A';
+    }
+    int DoD = 100 - currentBatteryLevel;
+    final hoursToFullCharge = (deviceBatteryCapacity * (DoD / 100)) /
+        (maxChargingRate * chargingEfficiency);
+
+    if (hoursToFullCharge.isInfinite || hoursToFullCharge.isNaN) {
+      return 'N/A';
+    }
+
+    final hours = hoursToFullCharge.floor();
+    final minutes = ((hoursToFullCharge - hours) * 60).round();
+
+    return '$hours ' +
+        LocaleData.hours.getString(context) +
+        ' $minutes ' +
+        LocaleData.minutes.getString(context);
   }
 }
